e0e47e5fb3fd608140acb976a7c6fdc7b29e97f5
igoryava <126491702+igoryava@users.noreply.github.com>
2025-12-19 23:17:46 +0700

    fix for shield ring

--- Assets/Source/Player/Items/Ring/Shield/ShieldRing.cs
+++ Assets/Source/Player/Items/Ring/Shield/ShieldRing.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ShieldRing : PlayerRing
 {
     [SerializeField] private GameObject _shieldGFX;
+    [SerializeField] private PlayerCheckOnEnemy _playerCheckOnEnemy;
+    [SerializeField] private float _checkRate = 0.02f;
     private float _defaultDamageMultiplier;
+    private bool _enemyDetected;
     private PlayerHitBox _playerHitBox;
+    private CompositeDisposable _disposable = new CompositeDisposable();
 
     public override void OnStartClient()
     {
@@ -19,17 +25,27 @@ public class ShieldRing : PlayerRing
     {
         _defaultDamageMultiplier = _playerHitBox.DamageMultiplier;
         _shieldGFX.SetActive(true);
-        _playerHitBox.DamageMultiplier = 0;
+        Observable.Interval(TimeSpan.FromSeconds(_checkRate)).Subscribe(_ =>
+        {
+            if (!_playerCheckOnEnemy.EnemyDetected())
+            {
+                _playerHitBox.DamageMultiplier = _defaultDamageMultiplier;
+                return;
+            }
+            _playerHitBox.DamageMultiplier = 0;
+        }).AddTo(_disposable);
     }
 
     protected override void OnRingAbilityBindCanceled(InputAction.CallbackContext obj)
     {
+        _disposable.Clear();
         _shieldGFX.SetActive(false);
         _playerHitBox.DamageMultiplier = _defaultDamageMultiplier;
     }
 
     protected override void CancelAction()
     {
+        _disposable.Clear();
         _shieldGFX.SetActive(false);
         _playerHitBox.DamageMultiplier = _defaultDamageMultiplier;
     }
